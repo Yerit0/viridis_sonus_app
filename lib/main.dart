ea3be8a2c7571ea9e30787a3a9e45f038111fdb0
YSilva <yeri.se17@hotmail.com>
2021-12-09 12:18:54 -0300

    creando splash screen y colores por defecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:viridis_sonus_app/routes.dart';
 import 'package:viridis_sonus_app/screens/home_screen.dart';
 import 'package:viridis_sonus_app/utils/theme/AppTheme.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
+
+
+// Dejamos en la instancia mas alta de la aplicacion
+// Los distintos servicios a consumir
+// Servicios alojados en la carpeta *Services*
+class AppState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+
+      ],
+      child: MyApp(),
+    );
+  }
+}
+
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppThemeData.lightTheme,
-      title: 'Material App',
+      title: 'Viridis Sonus',
       routes: routes(),
       home: HomeScreen()
     );
